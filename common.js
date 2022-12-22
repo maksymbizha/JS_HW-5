25486ca5b69aebdb10b855f5928990659c85e1ad
Maksym Bizhanov <103897410+maksymbizha@users.noreply.github.com>
2022-12-22 16:58:12 +0200

    update

--- common.js
+++ common.js
@@ -1,11 +1,12 @@
 const hour = +prompt('Enter number of hours?');
-let mathematicalOperation;
+
 
 if(!isNaN(hour)){
-    mathematicalOperation = hour * 3600;
+     const mathematicalOperation = hour * 3600;
     alert(`${hour} hour * 3600 = ${mathematicalOperation} second`);
+
 } else {
-    alert('Not a valid operation.')
+    alert('Not a valid number.')
 }
 
 
